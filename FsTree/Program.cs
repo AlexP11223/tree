fb478858a6e832484c3afa7643e45100fa10af10
Alex P <alex.pantec@gmail.com>
2019-07-11 00:40:20 +0300

    -a flag

--- FsTree/Program.cs
+++ FsTree/Program.cs
@@ -1,14 +1,50 @@
-﻿using System.Linq;
+﻿using System;
 
 namespace FsTree
 {
     static class Program
     {
+        static void ShowUsage()
+        {
+            Console.WriteLine("Usage: FsTree [dir] [-a]");
+            Console.WriteLine("  -a - show unix 'hidden' files/dirs like .git, .vs, .gitignore");
+        }
+
         static void Main(string[] args)
         {
-            string startDir = args.Any() ? args[0] : ".";
+            string startDir = null;
+            bool showAll = false;
+            // should use a library like https://github.com/commandlineparser/commandline but there are only 2 parameters for now, so whatever
+            foreach (var arg in args)
+            {
+                if (arg == "-a")
+                {
+                    showAll = true;
+                }
+                else
+                {
+                    if (startDir == null)
+                    {
+                        startDir = arg;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Unknown parameter {0}", arg);
+                        ShowUsage();
+                        return;
+                    }
+                }
+            }
 
-            new Tree(startDir).Print();
+            if (String.IsNullOrEmpty(startDir))
+            {
+                startDir = ".";
+            }
+
+            new Tree(startDir)
+            {
+                ShowAll = showAll
+            }.Print();
         }
     }
 }
